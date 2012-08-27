29db3fb3907bf9527b1cdd26f84851ea1f33db46
ben <ben@dreamerslab.com>
2012-08-27 15:59:46 +0800

    syntax tuning

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -5,17 +5,9 @@ var Application = require( CONTROLLER_DIR + 'application' );
 module.exports = Application.extend({
 
   init : function ( before, after ){
-    after( this.validation, {
-      only : [ 'create', 'update' ]
-    });
-
-    after( this.unique, {
-      only : [ 'create', 'update' ]
-    });
-
-    after( this.record_not_found, {
-      except : [ 'new', 'create', 'index' ]
-    });
+    after( this.validation,       { only : [ 'create', 'update' ]});
+    after( this.unique,           { only : [ 'create', 'update' ]});
+    after( this.record_not_found, { except : [ 'new', 'create', 'index' ]});
   },
 
   'new' : function ( req, res, next ){
@@ -25,10 +17,7 @@ module.exports = Application.extend({
   create : function ( req, res, next ){
     User.create_or_update( new User(), req.body.user,
       function ( err, user ){
-        if( err ){
-          next( err );
-          return;
-        }
+        if( err ) return next( err );
 
         req.flash( 'flash-info', 'User created' );
         res.redirect( '/users/' + user._id );
@@ -37,10 +26,7 @@ module.exports = Application.extend({
 
   index : function ( req, res, next ){
     User.find( function ( err, users ){
-      if( err ){
-        next( err );
-        return;
-      }
+      if( err ) return next( err );
 
       res.render( 'users/index', {
         users : users
@@ -51,11 +37,9 @@ module.exports = Application.extend({
   show : function ( req, res, next ){
     User.findById( req.params.id , function ( err, user ){
       if( user ){
-        res.render( 'users/show', {
+        return res.render( 'users/show', {
           user : user
         });
-
-        return;
       }
 
       req.msg = 'User';
@@ -66,11 +50,9 @@ module.exports = Application.extend({
   edit : function ( req, res, next ){
     User.findById( req.params.id , function ( err, user ){
       if( user ){
-        res.render( 'users/edit', {
+        return res.render( 'users/edit', {
           user : user
         });
-
-        return;
       }
 
       req.msg = 'User';
@@ -81,18 +63,13 @@ module.exports = Application.extend({
   update : function ( req, res, next ){
     User.findById( req.params.id , function ( err, user ){
       if( user ){
-        User.create_or_update( user, req.body.user,
+        return User.create_or_update( user, req.body.user,
           function ( err, user ){
-            if( err ){
-              next( err );
-              return;
-            }
+            if( err ) return next( err );
 
             req.flash( 'flash-info', 'User updated' );
             res.redirect( '/users/' + user._id );
           });
-
-        return;
       }
 
       req.msg = 'User';
@@ -103,17 +80,12 @@ module.exports = Application.extend({
   destroy : function ( req, res, next ){
     User.findById( req.params.id , function ( err, user ){
       if( user ){
-        user.remove( function ( err, user ){
-          if( err ){
-            next( err );
-            return;
-          }
+        return user.remove( function ( err, user ){
+          if( err ) return next( err );
 
           req.flash( 'flash-info', 'User deleted' );
           res.redirect( '/users' );
         });
-
-        return;
       }
 
       req.msg = 'User';
